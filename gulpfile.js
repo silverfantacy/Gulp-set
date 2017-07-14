2c694b27801b76122d9fed92f26b55a74dfd8336
Zero <silverfantacy@users.noreply.github.com>
2017-07-14 17:34:29 +0800

    初始

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+
+gulp.task('jade', function () {
+    gulp.src('./source/**/*.jade')
+        .pipe(plumber())
+        // .pipe(jade({
+        //     pretty: true // 無壓縮
+        // }))
+        .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('sass', function () {
+    var plugins = [
+        autoprefixer({
+            browsers: ['last 2 version', '> 5%', 'ie 8']
+        }),
+    ];
+    return gulp.src(['./source/scss/**/*.sass', './source/scss/**/*.scss'])
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        // 編譯完成 CSS
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./public/css'));
+});
+
+//監控
+gulp.task('watch', function () {
+    gulp.watch('./source/scss/**/*.scss', ['sass']);
+    gulp.watch('./source/scss/**/*.sass', ['sass']);
+    gulp.watch('./source/**/*.jade', ['jade']);
+});
+//依序執行
+gulp.task('default', ['jade', 'sass', 'watch']);
