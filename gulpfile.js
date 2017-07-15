0bdabbfb851e45871abda999d52003c7901a6918
Zero <silverfantacy@gmail.com>
2017-07-15 10:38:43 +0800

    加入ES6 編譯工具

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var autoprefixer = require('autoprefixer');
 gulp.task('jade', function () {
     gulp.src('./source/**/*.jade')
         .pipe($.plumber())
-        // .pipe($.jade({
-        //     pretty: true // 無壓縮
-        // }))
+        .pipe($.jade({
+            pretty: true // 無壓縮
+        }))
         .pipe(gulp.dest('./public/'))
 });
 
@@ -20,10 +20,24 @@ gulp.task('sass', function () {
     ];
     return gulp.src(['./source/scss/**/*.sass', './source/scss/**/*.scss'])
         .pipe($.plumber())
+        .pipe($.sourcemaps.init()) //顯示檔案位置
         .pipe($.sass().on('error', $.sass.logError))
         // 編譯完成 CSS
         .pipe($.postcss(plugins))
+        .pipe($.sourcemaps.write('.')) //顯示檔案位置
         .pipe(gulp.dest('./public/css'));
+});
+
+//ES6
+gulp.task('babel', () => {
+    return gulp.src('./source/js/**/*.js')
+        .pipe($.sourcemaps.init())//顯示檔案位置
+        .pipe($.babel({
+            presets: ['es2015']
+        }))
+        .pipe($.concat('all.js')) //合併js
+        .pipe($.sourcemaps.write('.')) //顯示檔案位置
+        .pipe(gulp.dest('./public/js'));
 });
 
 //監控
@@ -31,6 +45,7 @@ gulp.task('watch', function () {
     gulp.watch('./source/scss/**/*.scss', ['sass']);
     gulp.watch('./source/scss/**/*.sass', ['sass']);
     gulp.watch('./source/**/*.jade', ['jade']);
+    gulp.watch('./source/js/**/*.js', ['babel']);
 });
 //依序執行
-gulp.task('default', ['jade', 'sass', 'watch']);
+gulp.task('default', ['jade', 'sass', 'babel', 'watch']);
